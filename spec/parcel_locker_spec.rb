84f46977f4fc305aeb84f8a583725713a7880f12
bartlomiejh <bhopek@gmail.com>
2015-08-01 15:36:30 +0200

    remove initial failing test

--- spec/parcel_locker_spec.rb
+++ spec/parcel_locker_spec.rb
@@ -4,8 +4,4 @@ describe ParcelLocker do
   it 'has a version number' do
     expect(ParcelLocker::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
