32334b16b8dfca5d5ea746a7e8d743e4e54234ce
bartlomiejh <bhopek@gmail.com>
2015-08-02 17:12:48 +0200

    introduce API_URL module constant

--- lib/parcel_locker.rb
+++ lib/parcel_locker.rb
@@ -3,8 +3,10 @@ require 'faraday'
 require 'json'
 
 module ParcelLocker
+  API_URL = 'https://api-pl.easypack24.net/v4/machines?type=0'
+
   def self.all
-    response = Faraday.get('https://api-pl.easypack24.net/v4/machines?type=0')
+    response = Faraday.get(API_URL)
     # @todo: contract for json
     JSON.parse(response.body)['_embedded']['machines']
   end
