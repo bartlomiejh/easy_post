518a6c5286fb6945ae8e9221bed98e1a0d52215e
bartlomiejh <bhopek@gmail.com>
2015-08-02 17:01:00 +0200

    add rspec new syntax control

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,9 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'parcel_locker'
 require 'coveralls'
 Coveralls.wear!
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
